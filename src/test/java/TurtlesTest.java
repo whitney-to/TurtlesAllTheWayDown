947d7cce6b261bed4c08e97fae2ad7ba62173154
whitney-to <whitney012923@gmail.com>
2023-04-20 13:37:47 -0400

    added gcd test

--- src/test/java/TurtlesTest.java
+++ src/test/java/TurtlesTest.java
@@ -1,8 +1,33 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by kristofer on 7/14/20.
  */
 public class TurtlesTest {
+    @Test
+    public void turtlesTest1(){
+        // Given
+        Integer expected = 6;
 
+        // When
+        Integer actual = Turtles.gcd(12312312,234);
+
+        // Then
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void turtlesTest2(){
+        // Given
+        Integer expected = 6;
+
+        // When
+        Integer actual = Turtles.gcd2(12312312,234);
+
+        // Then
+        Assert.assertEquals(expected,actual);
+    }
 }
